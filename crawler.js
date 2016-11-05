7bc199853f2153a16279d6ef0bc335282ed97969
Jarob Gilliam <jarobgilliambiz@gmail.com>
2016-11-05 09:03:25 -0700

    Added a data configuration option which turns a string link into a meaningfully parsed object.`

--- crawler.js
+++ crawler.js
@@ -41,7 +41,8 @@ WebScraper.prototype.request = function(site) {
       // parse site_collection to strip out individual links.
       var temp = [];
       for (var key in site_collection) {
-        site_collection[key].attribs && temp.push(site_collection[key].attribs.href);
+        var reference = site_collection[key].attribs
+        reference && reference.href);
       }
 
       // CLEAN UP LINKS HERE
@@ -63,15 +64,82 @@ WebScraper.prototype.cleanUp = function(links) {
     console.log(item);
     for (var j = 0; j < args.length; j++) {
       var tester = args[j]
-      if (item && item.indexOf(tester) > -1) {
+      if (item && item.slice(0,4) === 'http') {
         improved.push(item);
       }
     }
   }
-
+  // Improved now contains an array of all links from the site of the form
+  // 'http...'. We are only scraping a single website, so many of these are
+  // unnecessary. We do want to collect them for data purposes, however.
+  
+  // IMPLEMENT DATA CONFIGURATION HERE
   console.log('Improved:', improved);
 
 }
+
+WebScraper.prototype.dataConfigure = function(links) {
+  var new_links = [];
+  for (var i = 0; i < links.length; i++) {
+
+    // variable reset
+    item = null;
+    parts = null;
+    suffix = null;
+    name = null;
+    identifier = null;
+    prefixes = null;
+    file_type = null;
+    file_name = null;
+    location = null;
+
+    var item = links[i];
+    var parts, left, right;
+    // We need to chop off the http:// or https://
+    item = item.split('//')[1];
+
+    if (item.indexOf('/') > -1) {
+      parts = item.split('/');
+      left = parts.shift();
+      right = parts.join('/');
+    } else {
+      left = item;
+      right = null;
+    }
+
+    left && (left = left.split('.'));
+    right && (right = right.split('/'));
+
+
+    var suffix = left.pop();
+    var name = left.pop();
+    var identifier = left.pop();
+    var prefixes = left;
+
+
+    var file_name, file_type;
+
+    if (right && right[right.length-1].indexOf('.') > -1) {
+      var file = right.pop();
+      file = file.split('.');
+      file_name = file[0];
+      file_type = file[1];
+    }
+    var location = right;
+    var obj  = {
+      'suffix' : suffix,
+      'name' : name,
+      'identifier' : identifier,
+      'prefixes' : prefixes,
+      'file_name' : file_name,
+      'file_type' : file_type,
+      'location' : location
+    };
+    new_links.push(obj);
+  }
+  return new_links;
+};
+  
 
 var scraper = new WebScraper();
 scraper.request();
